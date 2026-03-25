2cde0c53bc9a5d1fe0728f5d4eb4bb5c586280de
Minh Do <cl.nhatminh@gmail.com>
2026-03-26 02:33:41 +0700

    update

--- src/data/songs/index.ts
+++ src/data/songs/index.ts
@@ -1,23 +1,29 @@
 import type { Song } from "@/shared/types";
-import { song as fuGuang } from "./fu-guang";
-import { song as hotCrossBuns } from "./hot-cross-buns";
-import { song as lightlyRow } from "./lightly-row";
-import { song as littleWhiteCabbage } from "./little-white-cabbage";
-import { song as maryHadALittleLamb } from "./mary-had-a-little-lamb";
-import { song as odeToJoy } from "./ode-to-joy";
-import { song as onlyMamaIsGood } from "./only-mama-is-good";
-import { song as sweetHoney } from "./sweet-honey";
-import { song as theMoonRepresentsMyHeart } from "./the-moon-represents-my-heart";
-import { song as twinkleTwinkleLittleStar } from "./twinkle-twinkle-little-star";
+
+// Beginner (difficulty 1-2)
+import { song as hotCrossBuns } from "./beginner/hot-cross-buns";
+import { song as maryHadALittleLamb } from "./beginner/mary-had-a-little-lamb";
+import { song as lightlyRow } from "./beginner/lightly-row";
+import { song as twinkleTwinkleLittleStar } from "./beginner/twinkle-twinkle-little-star";
+import { song as onlyMamaIsGood } from "./beginner/only-mama-is-good";
+import { song as odeToJoy } from "./beginner/ode-to-joy";
+
+// Elementary (difficulty 3-4)
+import { song as littleWhiteCabbage } from "./elementary/little-white-cabbage";
+import { song as theMoonRepresentsMyHeart } from "./elementary/the-moon-represents-my-heart";
+import { song as sweetHoney } from "./elementary/sweet-honey";
+
+// Advanced (difficulty 7+)
+import { song as fuGuang } from "./advanced/fu-guang";
 
 export const songs: Song[] = [
-  maryHadALittleLamb,
   hotCrossBuns,
+  maryHadALittleLamb,
   lightlyRow,
   twinkleTwinkleLittleStar,
-  littleWhiteCabbage,
   onlyMamaIsGood,
   odeToJoy,
+  littleWhiteCabbage,
   theMoonRepresentsMyHeart,
   sweetHoney,
   fuGuang,
